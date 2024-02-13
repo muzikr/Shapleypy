e5570669e8acb6570ba2316c53e780f6e049d30d
Richard Muzik <richard@imuzik.cz>
2024-02-13 17:06:16 +0100

    feat: add csv loader of a game

--- src/shapleypy/constants.py
+++ src/shapleypy/constants.py
@@ -11,3 +11,6 @@ COALITION_NUMBER_OF_PLAYERS_ERROR = "Number of players must be between 1 and 32"
 LOADERS_MISSING_NUMBER_OF_PLAYERS_ERROR = (
     "The file does not contain the number of players."
 )
+LOADERS_CSV_SEPARATOR_ERROR = (
+    "csv_separator and coalition_separator cannot be the same"
+)
--- src/shapleypy/loaders.py
+++ src/shapleypy/loaders.py
@@ -1,8 +1,13 @@
 import ast
+import csv
 import json
 
-from shapleypy.constants import LOADERS_MISSING_NUMBER_OF_PLAYERS_ERROR
+from shapleypy.constants import (
+    LOADERS_CSV_SEPARATOR_ERROR,
+    LOADERS_MISSING_NUMBER_OF_PLAYERS_ERROR,
+)
 from shapleypy.game import Game
+
 
 def load_game_from_json(file: str) -> Game:
     n = None
@@ -18,6 +23,31 @@ def load_game_from_json(file: str) -> Game:
                 (ast.literal_eval(key), value)
                 for key, value in values_from_json.items()
             ]
+    game = Game(n)
+    game.set_values(values)
+    return game
+
+
+def load_game_from_csv(
+    file: str, csv_separator: str = ":", coalition_separator: str = ","
+) -> Game:
+    if csv_separator == coalition_separator:
+        raise ValueError(LOADERS_CSV_SEPARATOR_ERROR)
+
+    n = None
+    values = []
+    with open(file, newline="") as f:
+        reader = csv.reader(f, delimiter=csv_separator)
+        for row in reader:
+            if len(row) > 0:
+                if row[0] == "n":
+                    n = int(row[1])
+                else:
+                    key_list = list(map(int, row[0].split(coalition_separator)))
+                    value = float(row[1])
+                    values.append((key_list, value))
+    if n is None:
+        raise ValueError(LOADERS_MISSING_NUMBER_OF_PLAYERS_ERROR)
     game = Game(n)
     game.set_values(values)
     return game
