05787e4996b6b7324e983b90ad77a6a5560e2cda
Richard Muzik <richard@imuzik.cz>
2024-02-20 23:15:28 +0100

    feat: add possibility to save a game to csv file

--- src/shapleypy/savers.py
+++ src/shapleypy/savers.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import csv
 import json
 
 import numpy as np
 
 from shapleypy.coalition import Coalition
+from shapleypy.constants import CSV_SEPARATOR_ERROR
 from shapleypy.game import Game
 
 
@@ -26,3 +28,29 @@ def save_game_to_json(game: Game, filename: str) -> None:
 
     with open(filename, "w") as file:
         json.dump(prepared_game, file)
+
+
+def save_game_to_csv(
+    game: Game,
+    filename: str,
+    csv_separator: str = ":",
+    coalition_separator: str = ",",
+) -> None:
+    if csv_separator == coalition_separator:
+        raise ValueError(CSV_SEPARATOR_ERROR)
+
+    with open(filename, "w") as file:
+        writer = csv.writer(file, delimiter=csv_separator)
+        writer.writerow(["n", game.number_of_players])
+        for coalition, value in game.get_values(
+            Coalition.all_coalitions(game.number_of_players)
+        ):
+            if not np.isnan(value):
+                writer.writerow(
+                    [
+                        coalition_separator.join(
+                            map(str, coalition.get_players)
+                        ),
+                        value,
+                    ]
+                )
