09b32ee41c780aae2678009e46e2aa3534d97b29
Richard Muzik <richard@imuzik.cz>
2024-03-03 16:54:24 +0100

    test: add tests for core emptyness

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,41 @@
 from __future__ import annotations
 
 import numpy as np
+import pytest
 
 from shapleypy.coalition import Coalition
 from shapleypy.game import Game
-from shapleypy.solution_concept.core import _get_payoff, solution_in_core
+from shapleypy.solution_concept.core import (
+    _get_payoff,
+    is_empty,
+    solution_in_core,
+)
+
+
+@pytest.fixture
+def game_of_three_values_non_empty_core() -> list[tuple[Coalition, float]]:
+    return [
+        (Coalition.from_players([0]), 0.0),
+        (Coalition.from_players([1]), 0.0),
+        (Coalition.from_players([0, 1]), 0.25),
+        (Coalition.from_players([2]), 0.0),
+        (Coalition.from_players([0, 2]), 0.5),
+        (Coalition.from_players([1, 2]), 0.75),
+        (Coalition.from_players([0, 1, 2]), 1.0),
+    ]
+
+
+@pytest.fixture
+def game_of_three_values_empty_core() -> list[tuple[Coalition, float]]:
+    return [
+        (Coalition.from_players([0]), 0.0),
+        (Coalition.from_players([1]), 0.0),
+        (Coalition.from_players([0, 1]), 0.9),
+        (Coalition.from_players([2]), 0.0),
+        (Coalition.from_players([0, 2]), 0.9),
+        (Coalition.from_players([1, 2]), 0.9),
+        (Coalition.from_players([0, 1, 2]), 1.0),
+    ]
 
 
 def test_get_payoff() -> None:
@@ -23,3 +54,14 @@ def test_solution_in_core() -> None:
     assert solution_in_core(game, [0.5, 0.5])
     assert not solution_in_core(game, [0.5, 0.4])
     assert not solution_in_core(game, [1.0, 5.0])
+
+
+def test_is_empty(
+    game_of_three_values_non_empty_core: list[tuple[Coalition, float]],
+    game_of_three_values_empty_core: list[tuple[Coalition, float]],
+) -> None:
+    game = Game(3)
+    game.set_values(game_of_three_values_non_empty_core)
+    assert not is_empty(game)
+    game.set_values(game_of_three_values_empty_core)
+    assert is_empty(game)
