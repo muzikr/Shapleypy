f42db0cfeb6c2b591431eb2c7414487017d1a66e
Richard Muzik <richard@imuzik.cz>
2024-03-06 23:13:02 +0100

    perf: make class checkers little bit faster
    Slightly decreases number of searches in array and number of numerical
    operations.

--- src/shapleypy/classes/checkers.py
+++ src/shapleypy/classes/checkers.py
@@ -15,8 +15,9 @@ def check_monotonicity(game: Game) -> bool:
     """
     for S in game.all_coalitions:
         # Check the values of all subcoalitions of size |S| - 1
+        value_of_S = game.get_value(S)
         for S_minus_one in all_one_player_missing_subcoalitions(S):
-            if game.get_value(S) < game.get_value(S_minus_one):
+            if value_of_S < game.get_value(S_minus_one):
                 return False
     return True
 
@@ -24,18 +25,20 @@ def check_monotonicity(game: Game) -> bool:
 def check_weakly_superadditivity(game: Game) -> bool:
     grand_coalition = Coalition.grand_coalition(game.number_of_players)
     for i in range(game.number_of_players):
+        value_of_i = game.get_value([i])
         for S in (grand_coalition - i).all_subcoalitions():
-            if game.get_value(S) + game.get_value([i]) > game.get_value(S + i):
+            if game.get_value(S) + value_of_i > game.get_value(S + i):
                 return False
     return True
 
 
 def check_superadditivity(game: Game) -> bool:
     for T in game.all_coalitions:
+        value_of_T = game.get_value(T)
         for S in filter(
             lambda s: T * s == EMPTY_COALITION, game.all_coalitions
         ):
-            if game.get_value(T) + game.get_value(S) > game.get_value(T + S):
+            if value_of_T + game.get_value(S) > game.get_value(T + S):
                 return False
     return True
 
@@ -44,10 +47,12 @@ def check_convexity(game: Game) -> bool:
     grand_coalition = Coalition.grand_coalition(game.number_of_players)
     for i in range(game.number_of_players):
         for T in (grand_coalition - i).all_subcoalitions():
+            i_contribution_to_T = game.get_value(T + i) - game.get_value(T)
             for S in T.all_subcoalitions():
-                if game.get_value(S + i) - game.get_value(S) > game.get_value(
-                    T + i
-                ) - game.get_value(T):
+                if (
+                    game.get_value(S + i) - game.get_value(S)
+                    > i_contribution_to_T
+                ):
                     return False
     return True
 
