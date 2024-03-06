ba661c8c87d5b671c4005e845ca4fceee8a5316b
Richard Muzik <richard@imuzik.cz>
2024-03-06 18:15:40 +0100

    test: add tests for weakly superadditive checker

--- tests/test_classes_checkers.py
+++ tests/test_classes_checkers.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 import pytest
 
-from shapleypy.classes.checkers import check_monotone
+from shapleypy.classes.checkers import (
+    check_monotone,
+    check_weakly_superadditive,
+)
 from shapleypy.coalition import Coalition
 from shapleypy.game import Game
 
 
 @pytest.fixture
 def monotone_game_of_three() -> list[tuple[Coalition, float]]:
+    """Not weakly superadditive."""
     return [
         (Coalition.from_players([0]), 6.0),
         (Coalition.from_players([1]), 12.0),
@@ -20,6 +24,20 @@ def monotone_game_of_three() -> list[tuple[Coalition, float]]:
     ]
 
 
+@pytest.fixture
+def superadditive_game_of_three() -> list[tuple[Coalition, float]]:
+    """Superadditive, weakly superadditive"""
+    return [
+        (Coalition.from_players([0]), 6.0),
+        (Coalition.from_players([1]), 12.0),
+        (Coalition.from_players([0, 1]), 18.0),
+        (Coalition.from_players([2]), 42.0),
+        (Coalition.from_players([0, 2]), 48.0),
+        (Coalition.from_players([1, 2]), 55.0),
+        (Coalition.from_players([0, 1, 2]), 80.0),
+    ]
+
+
 def test_check_monotone(
     monotone_game_of_three: list[tuple[Coalition, float]]
 ) -> None:
@@ -28,3 +46,14 @@ def test_check_monotone(
     assert check_monotone(game)
     game.set_value(Coalition.from_players([0, 2]), 10.0)
     assert not check_monotone(game)
+
+
+def test_check_weakly_superadditive(
+    monotone_game_of_three: list[tuple[Coalition, float]],
+    superadditive_game_of_three: list[tuple[Coalition, float]],
+) -> None:
+    game = Game(3)
+    game.set_values(monotone_game_of_three)
+    assert not check_weakly_superadditive(game)
+    game.set_values(superadditive_game_of_three)
+    assert check_weakly_superadditive(game)
