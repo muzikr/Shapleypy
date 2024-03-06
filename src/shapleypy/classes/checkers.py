c7debc4025b9893d5b18fe48f34f9b3bb894118c
Richard Muzik <richard@imuzik.cz>
2024-03-06 18:14:31 +0100

    feat: add weak superadditivity game checker

--- src/shapleypy/classes/checkers.py
+++ src/shapleypy/classes/checkers.py
@@ -17,3 +17,14 @@ def check_monotonicity(game: Game) -> bool:
             ):
                 return False
     return True
+
+
+def check_weakly_superadditivity(game: Game) -> bool:
+    grand_coalition = Coalition.grand_coalition(game.number_of_players)
+    for i in range(game.number_of_players):
+        for coalition in (grand_coalition - i).all_subcoalitions():
+            if game.get_value(coalition) + game.get_value([i]) > game.get_value(
+                coalition + i
+            ):
+                return False
+    return True
