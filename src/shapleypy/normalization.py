6986b40b44e85c594098cf6e4e94daff3c2657d3
Richard Muzik <richard@imuzik.cz>
2024-03-09 20:12:10 +0100

    feat: add 0-1 normalization
    Subtracs values of all singletons from each coalition (makes singletons 0) and
    then divides by the value of grand coalition.

--- src/shapleypy/normalization.py
+++ src/shapleypy/normalization.py
@@ -11,3 +11,19 @@ def standart_normalization(game: Game) -> None:
         Coalition(2**game.number_of_players - 1)
     )
     game._values /= value_of_grand_coalition
+
+
+def zero_one_normalization(game: Game) -> None:
+    value_of_singletons = np.array(
+        [
+            game.get_value(Coalition.from_players([i]))
+            for i in range(game.number_of_players)
+        ]
+    )
+    for coalition in game.all_coalitions:
+        game.set_value(
+            coalition,
+            game.get_value(coalition)
+            - np.sum(value_of_singletons[list(coalition.get_players)]),
+        )
+    standart_normalization(game)
