77fdd5eeba18485627d7f7a41c7aa39e56f07e39
Richard Muzik <richard@imuzik.cz>
2024-03-09 15:19:14 +0100

    test: drop unnecessary fixture for class checkers

--- tests/test_classes_checkers.py
+++ tests/test_classes_checkers.py
@@ -17,7 +17,7 @@ from shapleypy.game import Game
 
 @pytest.fixture
 def monotone_game_of_three() -> list[tuple[Coalition, float]]:
-    """Not weakly superadditive, not supperadditive, not convex"""
+    """Not weakly superadditive"""
     return [
         (Coalition.from_players([0]), 6.0),
         (Coalition.from_players([1]), 12.0),
@@ -30,23 +30,8 @@ def monotone_game_of_three() -> list[tuple[Coalition, float]]:
 
 
 @pytest.fixture
-def superadditive_game_of_three() -> list[tuple[Coalition, float]]:
-    """Superadditive, weakly superadditive, positive"""
-    return [
-        (Coalition.from_players([0]), 6.0),
-        (Coalition.from_players([1]), 12.0),
-        (Coalition.from_players([0, 1]), 18.0),
-        (Coalition.from_players([2]), 42.0),
-        (Coalition.from_players([0, 2]), 48.0),
-        (Coalition.from_players([1, 2]), 55.0),
-        (Coalition.from_players([0, 1, 2]), 80.0),
-    ]
-
-
-@pytest.fixture
-def convex_game_of_three() -> list[tuple[Coalition, float]]:
+def positive_game_of_three() -> list[tuple[Coalition, float]]:
     """
-    Superadditive, weakly superadditive, convex, positive
     v(S)=|S|^2
     """
     return [
@@ -72,32 +57,32 @@ def test_check_monotonicity(
 
 def test_check_weakly_superadditivity(
     monotone_game_of_three: list[tuple[Coalition, float]],
-    superadditive_game_of_three: list[tuple[Coalition, float]],
+    positive_game_of_three: list[tuple[Coalition, float]],
 ) -> None:
     game = Game(3)
     game.set_values(monotone_game_of_three)
     assert not check_weakly_superadditivity(game)
-    game.set_values(superadditive_game_of_three)
+    game.set_values(positive_game_of_three)
     assert check_weakly_superadditivity(game)
 
 
 def test_check_superadditivity(
-    superadditive_game_of_three: list[tuple[Coalition, float]],
+    positive_game_of_three: list[tuple[Coalition, float]],
     monotone_game_of_three: list[tuple[Coalition, float]],
 ) -> None:
     game = Game(3)
-    game.set_values(superadditive_game_of_three)
+    game.set_values(positive_game_of_three)
     assert check_superadditivity(game)
     game.set_values(monotone_game_of_three)
     assert not check_superadditivity(game)
 
 
 def test_check_convexity(
-    convex_game_of_three: list[tuple[Coalition, float]],
+    positive_game_of_three: list[tuple[Coalition, float]],
     monotone_game_of_three: list[tuple[Coalition, float]],
 ) -> None:
     game = Game(3)
-    game.set_values(convex_game_of_three)
+    game.set_values(positive_game_of_three)
     assert check_convexity(game)
     assert check_supermodularity(game)
     game.set_values(monotone_game_of_three)
@@ -106,10 +91,10 @@ def test_check_convexity(
 
 
 def test_check_positivity(
-    convex_game_of_three: list[tuple[Coalition, float]],
+    positive_game_of_three: list[tuple[Coalition, float]],
 ) -> None:
     game = Game(3)
-    game.set_values(convex_game_of_three)
+    game.set_values(positive_game_of_three)
     assert check_positivity(game)
     # For S={0, 1, 2}, m_S = 0 so if I set whole to 8 it will be -1
     game.set_value(Coalition.from_players([0, 1, 2]), 8.0)
@@ -117,16 +102,13 @@ def test_check_positivity(
 
 
 def test_detemine_class(
-    convex_game_of_three: list[tuple[Coalition, float]],
+    positive_game_of_three: list[tuple[Coalition, float]],
     monotone_game_of_three: list[tuple[Coalition, float]],
-    superadditive_game_of_three: list[tuple[Coalition, float]],
 ) -> None:
     game = Game(3)
-    game.set_values(convex_game_of_three)
+    game.set_values(positive_game_of_three)
     assert determine_class(game) == "Positive"
     game.set_values(monotone_game_of_three)
     assert determine_class(game) == "Monotone"
-    game.set_values(superadditive_game_of_three)
-    assert determine_class(game) == "Positive"
     game.set_value(Coalition.from_players([0, 1, 2]), -8.0)
     assert determine_class(game) == "None"
